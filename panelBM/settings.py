27d3a4332debcbf663894b915ae6da8847518083
Bruno Morici M <bmorici@usm.cl>
2024-06-20 15:09:36 -0400

    agregamos getenv database_url en settings

--- panelBM/settings.py
+++ panelBM/settings.py
@@ -91,7 +91,8 @@ WSGI_APPLICATION = 'panelBM.wsgi.application'
 DATABASES = {
     # 'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3'}
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        # default='postgresql://postgres:postgres@localhost/postgres',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
 }
