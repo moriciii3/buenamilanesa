0a74441a8164e70b293f7838aaa9ae62b338cb89
Bruno Morici M <bmorici@usm.cl>
2024-06-20 15:22:10 -0400

    creamos base postgresql

--- panelBM/settings.py
+++ panelBM/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'panelBM.wsgi.application'
 DATABASES = {
     # 'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3'}
     'default': dj_database_url.config(
-        default='postgresql://db_buena_milanesa_user:1qcWXlUa1b3MFy3WsjMMHepLVs4ArKvS@dpg-cpq7ud9u0jms738r38sg-a:5432/db_buena_milanesa',
+        default='postgres://db_buena_milanesa_user:1qcWXlUa1b3MFy3WsjMMHepLVs4ArKvS@dpg-cpq7ud9u0jms738r38sg-a/db_buena_milanesa',
     )
 }
 
