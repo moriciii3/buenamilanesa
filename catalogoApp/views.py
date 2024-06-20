b10c126a330a3766a877dd3bc06cde587cbfe016
Bruno Morici M <bmorici@usm.cl>
2024-06-20 14:37:42 -0400

    First commit

--- catalogoApp/views.py
+++ catalogoApp/views.py
@@ -630,17 +630,6 @@ def enviarDespacho(request):
             return redirect('home')
         else:
             
-            # Credenciales de Twilio
-            auth_token = 'ef796500be917e2129ae12fafab9ab46'
-            account_sid = 'AC34c7c220577f11411ccd59c969e3a539'
-            client = Client(account_sid, auth_token)
-
-            message = client.messages.create(
-                from_='whatsapp:+14155238886',
-                body= request.POST['mensaje'],
-                to='whatsapp:+56973688125'
-            )
-            
             return redirect('calendarioPedidos')
         
 # Apartado de catalogo
